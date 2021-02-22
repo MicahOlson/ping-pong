d926036bbce1d977dde093fec774d450788edad0
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-02-22 11:05:29 -0800

    Add red-stage spec with good fail
    Co-authored-by: Kerry Lang <klang812@gmail.com>

--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -2,8 +2,8 @@ require('rspec')
 require('pry')
 require('ping_pong')
 
-# describe('#ping_pong') do
-#   it("") do
-#     expect(ping_pong("")).to(eq(""))
-#   end
-# end
+describe('#ping_pong') do
+  it("takes an integer and returns an array of integers in sequence from 1 to the inputted value") do
+    expect(ping_pong(5)).to(eq([1, 2, 3, 4, 5]))
+  end
+end
