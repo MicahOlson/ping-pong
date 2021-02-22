4cc75bc4526c47988db95399ec30da2fd6688545
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-02-22 13:22:48 -0800

    Add code to pass 'ping' test
    Co-authored-by: Kerry Lang <klang812@gmail.com>

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -1,3 +1,11 @@
 def ping_pong(number)
-  Array((1..number))
+  result = Array((1..number))
+  index = 0
+  result.each do |element|
+    if element.%(3).eql?(0)
+      result[index] = "ping"
+    end
+    index += 1
+  end
+  return result
 end
--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -4,10 +4,10 @@ require('ping_pong')
 
 describe('#ping_pong') do
   it("takes an integer and returns an array of integers in sequence from 1 to the inputted value") do
-    expect(ping_pong(5)).to(eq([1, 2, 3, 4, 5]))
+    expect(ping_pong(2)).to(eq([1, 2]))
   end
   
   it("will replace 'ping' for any number divisible by 3") do
-    expect(ping_pong(6)).to(eq([1, 2, "ping", 4, 5, "ping"]))
+    expect(ping_pong(4)).to(eq([1, 2, "ping", 4]))
   end
 end
