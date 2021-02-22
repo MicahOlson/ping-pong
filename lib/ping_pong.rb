ff06df0f5490b48506c19783452880b240677bac
Kerry Lang <klang812@gmail.com>
2021-02-22 13:55:19 -0800

    Add code to pass 'ping-pong' test
    Co-authored-by: Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -2,7 +2,9 @@ def ping_pong(number)
   result = Array((1..number))
   index = 0
   result.each do |element|
-    if element.%(3).eql?(0)
+    if (element.%(3).eql?(0) && element.%(5).eql?(0))
+      result[index] = "ping-pong"
+    elsif element.%(3).eql?(0)
       result[index] = "ping"
     elsif element.%(5).eql?(0)
       result[index] = "pong"
