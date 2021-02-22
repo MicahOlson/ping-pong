ef1c4d9f8fdc5ff9a5d8ec312a68182b0857921f
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-02-22 11:30:59 -0800

    Add 'ping' red-stage spec with good fail
    Co-authored-by: Kerry Lang <klang812@gmail.com>

--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -6,4 +6,8 @@ describe('#ping_pong') do
   it("takes an integer and returns an array of integers in sequence from 1 to the inputted value") do
     expect(ping_pong(5)).to(eq([1, 2, 3, 4, 5]))
   end
+  
+  it("will replace 'ping' for any number divisible by 3") do
+    expect(ping_pong(6)).to(eq([1, 2, "ping", 4, 5, "ping"]))
+  end
 end
